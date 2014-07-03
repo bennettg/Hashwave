cddc9ba25c4a1c5def39a47aaeed431bccdaa3c3
Bennett Gibson <bennett@sharetunnel.com>
2014-07-03 12:54:21 -0600

    Made own special iv separator randomizer

--- src/src/com/hashwave/MCrypt.java
+++ src/src/com/hashwave/MCrypt.java
@@ -4,6 +4,8 @@ import java.security.NoSuchAlgorithmException;
 
 
 
+
+
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
@@ -12,9 +14,9 @@ import javax.crypto.spec.SecretKeySpec;
 public class MCrypt {
 
         static char[] HEX_CHARS = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        private String iv = initIV();//"47fj568fjdk586kfj";//Dummy iv (CHANGE IT!)0
+        private IvParameterSpec ivspec; 
 
-        private String iv = initIV();//Dummy iv (CHANGE IT!)0
-        private IvParameterSpec ivspec;
         private SecretKeySpec keyspec;
         private Cipher cipher;
 
@@ -22,7 +24,6 @@ public class MCrypt {
 
         public MCrypt(String key)
         {
-                ivspec = new IvParameterSpec(iv.getBytes());
 
                 if(key!=null && !key.isEmpty()){
                 	this.SecretKey = key;
@@ -41,54 +42,75 @@ public class MCrypt {
                 }
         }
 
-        public byte[] encrypt(String text) throws Exception
-        {
+        public String encrypt(String text) throws Exception {
+            
+        	ivspec = new IvParameterSpec(iv.getBytes());
+        	
                 if(text == null || text.length() == 0)
                         throw new Exception("Empty string");
 
-                byte[] encrypted = null;
+                byte[] encryptedData = null;
+                String finalEncrypted = null;
 
                 try {
                         cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
 
-                        encrypted = cipher.doFinal(padString(text).getBytes());
+                        encryptedData = cipher.doFinal(padString(text).getBytes());
+                        
+                        finalEncrypted = MCrypt.bytesToHex(encryptedData);
+                        finalEncrypted = MCrypt.bytesToHex(((encryptedData +"_*()*_"+iv).getBytes()));
+
                 } catch (Exception e)
                 {                       
                         throw new Exception("[encrypt] " + e.getMessage());
                 }
 
-                return encrypted;
+                return finalEncrypted;
         }
 
-        public byte[] decrypt(String code) throws Exception
-        {
+        public String decrypt(String code) throws Exception {
                 if(code == null || code.length() == 0)
                         throw new Exception("Empty string");
 
-                byte[] decrypted = null;
+                String finalDecrypted = null;
+                String decryptedData = null;
+                byte[] decryptedE = null;
+                String thisiv;
 
-                try {
+                try {   
+                		byte[] decryptedA = hexToBytes(code);
+                		String decryptedB = new String(decryptedA, "UTF-8");;
+                		String[] decryptedC = code.split("_*()*_");	
+                        	decryptedData = decryptedC[0];
+                        	thisiv = decryptedC[1];
+                        	
+                        	
+                        ivspec = new IvParameterSpec(iv.getBytes());
+                        
                         cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
-
-                        decrypted = cipher.doFinal(hexToBytes(code));
+                        	
+                        decryptedE = cipher.doFinal(hexToBytes(decryptedData));
+  
                         //Remove trailing zeroes
-                        if( decrypted.length > 0)
+                        if( decryptedE.length > 0)
                         {
                             int trim = 0;
-                            for( int i = decrypted.length - 1; i >= 0; i-- ) if( decrypted[i] == 0 ) trim++;
+                            for( int i = decryptedE.length - 1; i >= 0; i-- ) if( decryptedE[i] == 0 ) trim++;
 
                             if( trim > 0 )
                             {
-                                byte[] newArray = new byte[decrypted.length - trim];
-                                System.arraycopy(decrypted, 0, newArray, 0, decrypted.length - trim);
-                                decrypted = newArray;
+                                byte[] newArray = new byte[decryptedE.length - trim];
+                                System.arraycopy(decryptedE, 0, newArray, 0, decryptedE.length - trim);
+                                decryptedE = newArray;
                             }
                         }
                 } catch (Exception e)
                 {
                         throw new Exception("[decrypt] " + e.getMessage());
                 }
-                return decrypted;
+                
+                finalDecrypted = new String(decryptedE,"UTF-8");
+                return finalDecrypted;
         }      
 
 
@@ -126,6 +148,14 @@ public class MCrypt {
 			RandomGen rand = new RandomGen();
 			iv = iv + rand.generateRandom(9, 16);
 			return iv;
+        }
+        
+        private static String appendSpecial() {
+        	String sp = "";
+        	
+        	RandomGen rand = new RandomGen();
+        	sp = sp + rand.generateRandom(9, 9);
+        	return sp;
         }
 
 
